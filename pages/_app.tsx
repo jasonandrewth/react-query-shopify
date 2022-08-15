ca7238b5f9563bb588bb22e1aa251ac0e6d545ac
jasonandrew <jasonxandrewth@gmail.com>
2022-08-15 13:55:45 +0200

    added getlayput check in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import {
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+import type { Page } from "src/types/page";
 import Layout from "components/Layout";
 
 import NextApp, { AppProps } from "next/app";
@@ -14,16 +15,24 @@ import NextApp, { AppProps } from "next/app";
 import "../styles/globals.css";
 import "swiper/scss";
 
+// this should give a better typing
+type Props = AppProps & {
+  Component: Page;
+};
+
 function App({
   Component,
   pageProps: { session, dehydratedState, ...pageProps },
-}: AppProps) {
+}: Props) {
   const [queryClient] = useState(() => new QueryClient());
+
+  // If the component has a getLayout() function, use it. Otherwise just render the page as is.
+  const getLayout = Component.getLayout || ((page) => page);
 
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={dehydratedState}>
-        <Layout main={<Component {...pageProps} />} />
+        {getLayout(<Component {...pageProps} />)}
       </Hydrate>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
