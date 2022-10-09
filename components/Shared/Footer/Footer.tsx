9813ebac1287d2ff5a7f49617b29132ad7209057
jasonandrew <jasonxandrewth@gmail.com>
2022-10-09 14:09:25 +0200

    Patreon Link

--- components/Shared/Footer/Footer.tsx
+++ components/Shared/Footer/Footer.tsx
@@ -9,13 +9,19 @@ const Footer = () => {
     <footer className="fixed bottom-0 left-0 z-50 w-screen bg-black text-white grid grid-cols-[auto_auto]">
       <div className="flex border-r border-r-white p-4">
         <button className="hover:opacity-80 mr-2">
-          <Patreon className="h-5 w-6" />
+          <a
+            href="https://www.patreon.com/ben_ditto"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Patreon className="h-5 w-6" />
+          </a>
         </button>
         <button className="hover:opacity-80 mr-2">
           <img className="max-h-6" src="/instagram.svg" alt="Instagram Logo" />
         </button>
         <button className="hover:opacity-80">
-          <img className="max-h-6" src="/discord.svg" alt="Instagram Logo" />
+          <img className="max-h-6" src="/discord.svg" alt="Discord Logo" />
         </button>
       </div>
 
