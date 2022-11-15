5d11c808d2dc752356d7fe56e5d343710a92e553
jasonandrew <jasonxandrewth@gmail.com>
2022-11-15 14:55:12 +0100

    Ts ignore try

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { DefaultSeo } from "next-seo";
 import config from "config/seo.json";
 
@@ -22,50 +22,22 @@ type Props = AppProps & {
   Component: Page;
 };
 
-function App({
-  Component,
-  router,
-  pageProps: { session, dehydratedState, ...pageProps },
-}: Props) {
+function App({ Component, router, pageProps }: Props) {
   const [queryClient] = useState(() => new QueryClient());
 
   // If the component has a getLayout() function, use it. Otherwise just render the page as is.
   const getLayout = Component.getLayout || ((page) => page);
 
-  useEffect(() => {
-    console.log(
-      `  
-           .o######0o.
-          0###########0.      .
-         o####" "######0.    (## m#o
-         ####(    ######0  ._ ##.##"nn
-         0####o   ###" ## (##o.######"
-  o00o.    0#####o,##. ,#"  "#######(
-  .0#####0.   0###########0     ########
-  .0#######0.   "0#########"  _.o###'"00"
-  .0###########o._ ""################       _  .
-  0####" "#########################0      .0#0n0
-  #####.   ""#####################"    _  0#####
-  0#####.     "###################._.o##o.#####"
-  "0#####..##mn ""#############################
-  "0#######""_    ""##################"#####"
-  ""####m###m      ""############"   ####
-  .########"""         .########"     "##"
-  ####"##"###o        (0######"        ""
-  "##".###,##     .o#o ""####.
-      "##"      .0############.
-              .n##======####### 
-   
-   WEB BY JASON ANDREW jason-andrew.com
-   `
-    );
-  }, []);
-
   const { asPath } = useRouter();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Hydrate state={dehydratedState}>
+      <Hydrate
+        state={
+          //@ts-ignore
+          pageProps.dehydratedState
+        }
+      >
         <DefaultSeo
           {...config}
           // hello
